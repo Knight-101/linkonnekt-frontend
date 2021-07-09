1ca57483aaf35d53e781fef7112b4ed649bd8deb
dakshisdaksh <9897652880dag@gmail.com>
2021-07-09 19:59:12 +0530

    basic changes

---
+++ src/components/ProfileInfo/Freelancer/GraphicDesigner.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const  GraphicDesigner= () => {
+     return(
+        <div>
+              Graphic Designer Prop is called
+        </div>
+     );
+};
+
+export default GraphicDesigner;
---
+++ src/components/ProfileInfo/Freelancer/VideoEditor.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect, useRef } from "react";
+import InputLabel from "@material-ui/core/InputLabel";
+import NativeSelect from "@material-ui/core/NativeSelect";
+import { Checkbox } from '@material-ui/core';
+
+const VideoEditor = () => {
+    const softwares= [ 'PremierePro', 'AfterEffects', 'Filmora'];
+     return(
+        <div>
+            <InputLabel
+            shrink
+            htmlFor="age-native-label-placeholder"
+            style={{ textAlign: "left" }}
+          >
+            Select Category
+          </InputLabel>   
+            
+        </div>
+     );
+};
+
+export default VideoEditor;
