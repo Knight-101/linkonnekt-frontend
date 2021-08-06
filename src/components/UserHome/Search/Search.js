15f67da0296abf37f4f23a22f661a57169ac1ceb
Sanskar Mohapatra <sanskarmohapatra@gmail.com>
2021-08-06 15:51:41 +0530

    pagination

--- src/components/UserHome/Search/Search.js
+++ src/components/UserHome/Search/Search.js
@@ -6,6 +6,7 @@ import Filters from "./Filters";
 import axios from "axios";
 import { css } from "@emotion/react";
 import ClipLoader from "react-spinners/ClipLoader";
+import Pagination from "@material-ui/lab/Pagination";
 
 const override = css`
   display: block;
@@ -21,18 +22,27 @@ function Search(props) {
   const [creatorsArray, setcreatorsArray] = useState([]);
   // const email = useSelector((state) => state.userData.email);
   const [loading, setloading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [pageNo, setPageNo] = useState(1);
 
   useEffect(() => {
     axios
       .get(BASE_URL + "/creator/list")
       .then(async (res) => {
         setcreatorsArray(res.data);
+        const len = res.data.length;
+        setPageNo(Math.floor(len / 10) + 1);
         setloading(false);
       })
       .catch((err) => {
         console.log(err);
       });
   }, [BASE_URL]);
+
+  const handleChange = (event, value) => {
+    setPage(value);
+    window.scrollTo(0, 0);
+  };
   return (
     <div>
       {loading ? (
@@ -56,28 +66,38 @@ function Search(props) {
                 style={{ display: "flex", flexDirection: "Column" }}
               >
                 {creatorsArray &&
-                  creatorsArray.map(
-                    (creator, index) =>
-                      creator.profileInfo && (
-                        <UserCard
-                          key={index}
-                          name={
-                            creator.profileInfo.personalInfo.firstName +
-                            " " +
-                            creator.profileInfo.personalInfo.lastName
-                          }
-                          category={creator.profileInfo.categories.Category}
-                          image={creator.profileImg}
-                          about={creator.profileInfo.personalInfo.about}
-                          YouTube={creator.profileInfo.socialLinks.YouTube}
-                          LinkedIn={creator.profileInfo.socialLinks.LinkedIn}
-                          Instagram={creator.profileInfo.socialLinks.Instagram}
-                          Facebook={creator.profileInfo.socialLinks.Facebook}
-                          Twitter={creator.profileInfo.socialLinks.Twitter}
-                          username={creator.username}
-                        />
-                      )
-                  )}
+                  creatorsArray
+                    .slice(page * 10 - 10, page * 10)
+                    .map(
+                      (creator, index) =>
+                        creator.profileInfo && (
+                          <UserCard
+                            key={index}
+                            name={
+                              creator.profileInfo.personalInfo.firstName +
+                              " " +
+                              creator.profileInfo.personalInfo.lastName
+                            }
+                            category={creator.profileInfo.categories.Category}
+                            image={creator.profileImg}
+                            about={creator.profileInfo.personalInfo.about}
+                            YouTube={creator.profileInfo.socialLinks.YouTube}
+                            LinkedIn={creator.profileInfo.socialLinks.LinkedIn}
+                            Instagram={
+                              creator.profileInfo.socialLinks.Instagram
+                            }
+                            Facebook={creator.profileInfo.socialLinks.Facebook}
+                            Twitter={creator.profileInfo.socialLinks.Twitter}
+                            username={creator.username}
+                          />
+                        )
+                    )}
+                <hr></hr>
+                <Pagination
+                  count={pageNo}
+                  page={page}
+                  onChange={handleChange}
+                />
               </div>
             ) : (
               <div>
